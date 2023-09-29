bc57232c10fc181bef15e2b02dafdb8e646e7d6c
Jayanth0319 <143284365+Jayanth0319@users.noreply.github.com>
2023-09-29 12:01:07 -0500

    Update ex_4_2.py

--- src/ex_4_2.py
+++ src/ex_4_2.py
@@ -3,11 +3,8 @@ from datetime import datetime
 
 
 def logstamp_to_datetime(datestr):
-    """
-    Your docstring here.  Replace the pass keyword below with your implementation.
-    """
-    pass
-
+    datestr_format = '%Y-%m-%dT%H:%M:%S'
+    return datetime.strptime(datestr, datestr_format)
 
 # >>>> The code below will call your function and print the results
 if __name__ == "__main__":
